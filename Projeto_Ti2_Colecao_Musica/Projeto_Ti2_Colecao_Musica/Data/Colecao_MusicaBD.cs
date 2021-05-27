64ffe6cb000022c14c2b7a9697f4cd94f5fe9275
Jose Silva <aluno20796@ipt.pt>
2021-05-27 23:55:44 +0100

    Localização base de dados

---
+++ Projeto_Ti2_Colecao_Musica/Projeto_Ti2_Colecao_Musica/Data/Colecao_MusicaBD.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projeto_Ti2_Colecao_Musica.Models
+
+namespace Projeto_Ti2_Colecao_Musica.Data
+{
+    /// <summary>
+    /// representa a DB da colecao de musica
+    /// </summary>
+    public class Colecao_MusicaBD :DbContext {
+
+        //*********************************************
+        //especificar as tabelas na BD
+        //*********************************************
+        public DbSet<Musicas> Musicas { get; set; }
+        public DbSet<Albuns> Albuns { get; set; }
+        public DbSet<Artistas> Artistas { get; set; }
+        public DbSet<Generos> Generos { get; set; }
+        //*********************************************
+    }
+
+}
